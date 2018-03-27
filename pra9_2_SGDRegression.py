f147d76d09713372747083d9b62e3f06abc0a9e8
nmaypeter <nmaypeter3@gmail.com>
2018-03-27 11:54:53 +0800

    SGDRegression

---
+++ pra9_2_SGDRegression.py
@@ -0,0 +1,11 @@
+import numpy as np
+from sklearn import linear_model
+
+n_samples, n_features = 10, 5
+np.random.seed(0)
+y = np.random.randn(n_samples)
+X = np.random.randn(n_samples, n_features)
+clf = linear_model.SGDRegressor()
+clf.fit(X, y)
+print("y", y)
+print("prediction:", clf.predict(X))
